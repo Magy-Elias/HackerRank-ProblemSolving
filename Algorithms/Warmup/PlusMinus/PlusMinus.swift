4406d2794e59cbfc0ef063a6d8f9c358dda297e2
Magy Elias <melias@iotblue.net>
2020-10-28 12:04:14 +0200

    - Add the copyright - Rename the swift files

--- Algorithms/Warmup/PlusMinus/PlusMinus.swift
+++ Algorithms/Warmup/PlusMinus/PlusMinus.swift
@@ -1,3 +1,11 @@
+//
+//  PlusMinus.swift
+//  HackerRank-ProblemSolving
+//
+//  Created by Magy Elias on 27/01/20.
+//  Copyright © 2020 Magy Elias. All rights reserved.
+//
+
 import UIKit
 import Foundation
 
